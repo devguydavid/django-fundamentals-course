ee0f10ae1c85958430077651299fc5748fb9bc6e
David M. Brown <david@davtar.org>
2018-08-20 23:07:52 -0600

    Show active games for current user

--- player/views.py
+++ player/views.py
@@ -4,7 +4,8 @@ from gameplay.models import Game
 
 
 def home(request):
+    my_games = Game.objects.games_for_user(request.user)
+    active_games = my_games.active()
     return render(request,
                   "player/home.html",
-                  {'ngames': Game.objects.count()}
-    )
+                  {'games': active_games})
